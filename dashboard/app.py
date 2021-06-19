ac9fc5e7229dfa24fb51d159e0ae15089f8f1175
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-06-19 11:45:39 +0100

    changes

--- dashboard/app.py
+++ dashboard/app.py
@@ -11,7 +11,6 @@ import plotly.express as px
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
